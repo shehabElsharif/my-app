2128b0f67fd2f1a12d35cf01b75508645a20432a
shehab <shehab.m.b.elsharif@gmail.com>
2022-11-08 22:01:41 +0200

    problem with react had to reinstall and startagain

---
+++ src/Content.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState } from 'react';
+import {FaTrashAlt} from 'react-icons/fa'
+
+const Content = () => {
+/*   const [name, setName] = useState('shehab');
+    const handleNameChange =() =>{
+        const name = ['bob', 'shehab', 'boushra'];
+        const int = Math.floor(Math.random()*3);
+        setName(name[int]);
+        return name[int];
+      }
+      
+      const handleClick = () =>{
+        const name = handleNameChange();
+        console.log(name);
+        } */
+const [items, setItems] = useState([
+  {
+    id: 1,
+    checked: true,
+    item: 'One half pound bag of Cocoa Covered Almondes Unsalted'
+  },
+  {
+    id: 2,
+    checked: false,
+    item: 'One half pound bag of Cocoa Covered Almondes Unsalted'
+  },
+  {
+    id: 3,
+    checked: false,
+    item: 'One half pound bag of Cocoa Covered Almondes Unsalted'
+  },
+])
+
+  return (
+    <main>
+      <ul>
+      {items.map((item) =>
+        <li className ="item" key={item.id}>
+          <input 
+          type="checkbox" 
+          checked="{item.checked}"
+          />
+          <label>{item.item}</label>
+          <FaTrashAlt 
+            role="button" 
+            tabIndex="0"
+            />
+        </li>
+
+      )}
+    </ul>
+    </main>
+  )
+  
+}
+
+export default Content
